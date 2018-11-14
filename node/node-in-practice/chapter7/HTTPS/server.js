2f1d982dc9f838cae56a590a60f5233970e86589
luoshilu <978337784@qq.com>
2018-11-15 01:27:46 +0800

    TCP/HTTP + TLS

---
+++ node/node-in-practice/chapter7/HTTPS/server.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+var https = require('https');
+
+var  options = {
+  key: fs.readFileSync('server.pem'),
+  cert: fs.readFileSync('server-cert.pem'),
+  ca: [fs.readFileSync('client-cert.pem')],
+  requestCert: true
+};
+
+var server = https.createServer(options, function(req, res){
+  var authorized = req.socket.authorized?'authorized' : 'unauthorized';
+  res.writeHead(200);
+  res.write('Welcome ! you are'+ authorized + '\n');
+  res.end();
+});
+
+server.listen('8000', function(){
+  console.log('server listening');
+});
